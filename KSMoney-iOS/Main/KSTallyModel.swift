7ea85b603dbbcb9071667f0aa7a8eadab6aef5df
xiaoshi <sjl_leaf@163.com>
2017-07-26 18:52:29 +0800

    添加拉取流水的接口

---
+++ KSMoney-iOS/Main/KSTallyModel.swift
@@ -0,0 +1,14 @@
+//
+//  KSTallyModel.swift
+//  KSMoney-iOS
+//
+//  Created by xiaoshi on 2017/7/26.
+//  Copyright © 2017年 kamy. All rights reserved.
+//
+
+import UIKit
+import HandyJSON
+
+struct KSTallyModel: HandyJSON {
+
+}
--- KSMoney-iOS/Tool/KSNetTool.swift
+++ KSMoney-iOS/Tool/KSNetTool.swift
@@ -15,27 +15,31 @@ import SwiftyUserDefaults
 struct KSNetTool {
     static func loginWithParams(_ params: [String: Any]? = nil, success : @escaping (_ response : KSLoginModel)->(), failure : @escaping (_ error : Error)->()) {
         Alamofire.request(ApiLogin.tokenUrl(), method: .post, parameters: params).responseJSON { (response) in
-            print("Request: \(String(describing: response.request))")   // original url request
-            print("Response: \(String(describing: response.response))") // http url response
-            print("Result: \(response.result)")                         // response serialization result
-
-            if let json = response.result.value {
-
-                print("JSON: \(json)") // serialized json response
-            }
-
-            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)") // original server data as UTF8 string
-            }
             switch response.result {
-            case .success(let data):
+            case .success:
                 if let data = response.result.value as? NSDictionary {
                     let model = KSLoginModel.deserialize(from: data)
                     Defaults[.token] = model?.token
                     //Defaults.set(model, forKey: "loginModel")
                     success(model!)
+                } else {
+                    //failure()
                 }
-                print(data)
+            case .failure(let error):
+                failure(error)
+                print("error:\(error)")
+            }
+        }
+    }
+
+    static func getTallys(_ params: [String: Any], success: @escaping (_ response: [KSTallyModel])->(), failure: @escaping (_ eror: Error)->()) {
+        Alamofire.request(ApiTally.tokenUrl(), method: .get, parameters: params).responseJSON { (response) in
+            switch response.result {
+            case .success:
+                if let data = response.result.value as? NSDictionary {
+                    let tallys = [KSTallyModel].deserialize(from: (data["tallys"] as! NSArray))
+                    success(tallys! as! [KSTallyModel])
+                }
             case .failure(let error):
                 failure(error)
                 print("error:\(error)")
@@ -44,3 +48,19 @@ struct KSNetTool {
     }
 }
 
+
+
+/*
+ print("Request: \(String(describing: response.request))")   // original url request
+ print("Response: \(String(describing: response.response))") // http url response
+ print("Result: \(response.result)")                         // response serialization result
+
+ if let json = response.result.value {
+
+ print("JSON: \(json)") // serialized json response
+ }
+
+ if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+ print("Data: \(utf8Text)") // original server data as UTF8 string
+ }
+ */
