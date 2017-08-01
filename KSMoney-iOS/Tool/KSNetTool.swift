dc7a4cb4dfeea641c8aea7fd946161df1a136aef
xiaoshi <sjl_leaf@163.com>
2017-08-01 19:05:41 +0800

    接口修改，封装接口

--- KSMoney-iOS/Tool/KSNetTool.swift
+++ KSMoney-iOS/Tool/KSNetTool.swift
@@ -12,6 +12,9 @@ import Alamofire
 import HandyJSON
 import SwiftyUserDefaults
 
+private let failureStr: String = "接口失败"
+private let lackKeyErrorStr: String = "缺少字段"
+
 struct KSNetTool {
     static func loginWithParams(_ params: [String: Any]? = nil, success : @escaping (_ response : KSLoginModel)->(), failure : @escaping (_ error : Error)->()) {
         Alamofire.request(ApiLogin.tokenUrl(), method: .post, parameters: params).responseJSON { (response) in
@@ -32,16 +35,57 @@ struct KSNetTool {
         }
     }
 
-    static func getTallys(_ params: [String: Any], success: @escaping (_ response: [KSTallyModel])->(), failure: @escaping (_ eror: Error)->()) {
-        Alamofire.request(ApiTally.tokenUrl(), method: .get, parameters: params).responseJSON { (response) in
+    static func getTallys(_ params: [String: Any], success: @escaping (_ response: [KSTallyModel])->(), failure: @escaping (_ error: String)->()) {
+        request(ApiTally.tokenUrl(), method: .get, parameters: params, success: { (response) in
+            if let data = response as? [String : Any] {
+                let tallys = [KSTallyModel].deserialize(from: (data["tallys"] as! NSArray))
+                success(tallys! as! [KSTallyModel])
+            } else {
+                failure(lackKeyErrorStr)
+            }
+        }) { (error) in
+            failure(error)
+        }
+
+    }
+
+    static func request(_ url: URLConvertible,
+                        method: HTTPMethod = .get,
+                        parameters: Parameters? = nil,
+                        encoding: ParameterEncoding = URLEncoding.default,
+                        headers: HTTPHeaders? = nil,
+                        success: @escaping (_ response: Any)->(),
+                        failure: @escaping (_ error: String)->()) {
+        Alamofire.request(url, method: method, parameters: parameters, encoding: encoding, headers: headers).responseJSON { (response) in
             switch response.result {
             case .success:
-                if let data = response.result.value as? NSDictionary {
-                    let tallys = [KSTallyModel].deserialize(from: (data["tallys"] as! NSArray))
-                    success(tallys! as! [KSTallyModel])
+                if let data = response.result.value as? [String : Any] {
+                    let code = data["code"] as! Int
+                    if code == 0 {
+                        success(data)
+                    } else {
+                        if let msg = data["message"] as? String {
+                            if msg == "未登录" {
+                                Defaults[.token] = nil
+                                let window : UIWindow = (UIApplication.shared.delegate?.window!!)!
+                                DispatchQueue.main.async {
+                                    if !window.isKeyWindow {
+                                        window.makeKeyAndVisible()
+                                    }
+                                    window.rootViewController = UINavigationController(rootViewController: KSLoginController())
+                                }
+                            } else {
+                                failure(msg)
+                            }
+                        } else {
+                            failure(failureStr)
+                        }
+                    }
+                } else {
+                    failure(failureStr)
                 }
             case .failure(let error):
-                failure(error)
+                failure("error:\(error)")
                 print("error:\(error)")
             }
         }
