75312bc0e2c4a7c93c64c75fef8eb0b990747c64
xiaoshi <sjl_leaf@163.com>
2017-08-02 17:13:33 +0800

    数据处理，价格千位分割

--- KSMoney-iOS/Main/View/KSTallyCell.swift
+++ KSMoney-iOS/Main/View/KSTallyCell.swift
@@ -15,6 +15,8 @@ class KSTallyCell: UITableViewCell {
     @IBOutlet weak var remarkLabel: UILabel!
     @IBOutlet weak var moneyLabel: UILabel!
 
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weekLabel: UILabel!
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -33,7 +35,18 @@ class KSTallyCell: UITableViewCell {
             } else {
                 categoryLabel.text = model?.category?.name
             }
-            
+            moneyLabel.text = (model?.price)!.formatPrice()
+            if (model?.in_account_id)! > 0 && (model?.out_account_id)! > 0 {
+                moneyLabel.textColor = UIColor(hex: "00B5FF")
+                self.typeIcon.image = UIImage(named: "icon_transfer")
+            } else if (model?.in_account_id)! > 0 {
+                moneyLabel.textColor = UIColor(hex: "FF6710")
+                self.typeIcon.image = UIImage(named: "icon_income")
+            } else {
+                moneyLabel.textColor = UIColor(hex: "999999")
+                self.typeIcon.image = UIImage(named: "icon_expend")
+            }
+            remarkLabel.text = (model?.remark)!
         }
     }
 
